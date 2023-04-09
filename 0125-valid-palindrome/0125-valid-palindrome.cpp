5b8a72fe0c3cae2169d20b60bb0090a30694fb71
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2023-04-09 19:02:58 +0200

    Time: 7 ms (49.83%), Space: 7.7 MB (46.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -13,22 +13,22 @@ public:
         }
         int n=ss.size();
         bool palindrome=true;
-        string reversed=ss;
-        reverse(reversed.begin(),reversed.end());
-        cout<<reversed<<endl;
-        for(int i=0; i<n; i++)
-        {
-            if(ss[i]!=reversed[i])
-            {
-                palindrome=false;
-                break;
-            }
-        }
-        //  for(int i=0,j=1; i<n/2; i++,j++)
+        // string reversed=ss;
+        // reverse(reversed.begin(),reversed.end());
+        // cout<<reversed<<endl;
+        // for(int i=0; i<n; i++)
         // {
-        //     if(ss[i]==ss[n-j])continue;
-        //     else palindrome=false;
+        //     if(ss[i]!=reversed[i])
+        //     {
+        //         palindrome=false;
+        //         break;
+        //     }
         // }
+         for(int i=0,j=1; i<n/2; i++,j++)
+        {
+            if(ss[i]==ss[n-j])continue;
+            else palindrome=false;
+        }
         return palindrome;
     }
 };
