f23afddc7aeaf9b710c6c014911730a9dc05ec58
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2023-04-10 17:15:49 +0200

    Time: 3 ms (52.84%), Space: 5.9 MB (62.30%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        long long power_of_two[40];
-        power_of_two[0]=1;
-        for(int i=1; i<=32; i++)
-        {   
-            if(power_of_two[i-1]==n)return true;
-            power_of_two[i]=power_of_two[i-1]*2;
-        }
-        return false;
+     if(n==0) return 0;
+     if(n==1) return true;
+     return n%2==0&&isPowerOfTwo(n/2);
+    
     }
 };
