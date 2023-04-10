7df024500c1095e06ced328a673f95eee59d282b
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2023-04-10 02:05:20 +0200

    Time: 622 ms (68.54%), Space: 182.4 MB (47.85%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,49 +1,41 @@
 class Solution {
 public:
-    int dfs(int node, string& colors, vector<vector<int>>& adj, vector<vector<int>>& count,
-            vector<bool>& visit, vector<bool>& inStack) {
-        // If the node is already in the stack, we have a cycle.
-        if (inStack[node]) {
-            return INT_MAX;
-        }
-        if (visit[node]) {
-            return count[node][colors[node] - 'a'];
-        }
-        // Mark the current node as visited and part of current recursion stack.
-        visit[node] = true;
-        inStack[node] = true;
-
-        for (auto& neighbor : adj[node]) {
-            if (dfs(neighbor, colors, adj, count, visit, inStack) == INT_MAX) {
-                return INT_MAX;
-            }
-            for (int i = 0; i < 26; i++) {
-                count[node][i] = max(count[node][i], count[neighbor][i]);
-            }
-        }
-
-        // After all the incoming edges to the node are processed,
-        // we count the color on the node itself.
-        count[node][colors[node] - 'a']++;
-        // Remove the node from the stack.
-        inStack[node] = false;
-        return count[node][colors[node] - 'a'];
-    }
-
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.length();
-        vector<vector<int>> adj(n);
-        for (auto& edge : edges) {
+        int n=colors.size();
+        vector<vector<int>>adj(n);
+        vector<bool>vis(n),instack(n);
+        vector<vector<int>>count(n,vector<int>(26));
+        // for(int i=0; i<edges.size(); i++)
+        // {
+        //     adj[edges[i][0]].push_back(edges[i][1]);
+        // }
+         for (auto& edge : edges) {
             adj[edge[0]].push_back(edge[1]);
         }
-
-        vector<vector<int>> count(n, vector<int>(26));
-        vector<bool> visit(n), inStack(n);
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = max(answer, dfs(i, colors, adj, count, visit, inStack));
+        int ans=0;
+        for(int i=0; i<n; i++)
+        {
+            ans=max(ans,dfs(i,vis,instack,count,colors,adj));
         }
-
-        return answer == INT_MAX ? -1 : answer;
+        if(ans==INT_MAX)ans=-1;
+        return ans;
+    }
+    int dfs(int node,vector<bool>&vis,vector<bool>&instack,vector<vector<int>>&count,string &colors,vector<vector<int>>&adj)
+    {
+        if(instack[node])return INT_MAX;
+        if(vis[node])return count[node][colors[node]-'a'];
+        vis[node]=true;
+        instack[node]=true;
+        for(int i=0; i<adj[node].size(); i++)
+        {
+            if(dfs(adj[node][i],vis,instack,count,colors,adj)==INT_MAX)return INT_MAX;
+            for(int j=0; j<26; j++)
+            {
+                count[node][j]=max(count[node][j],count[adj[node][i]][j]);
+            }
+        }
+        instack[node]=false;
+        count[node][colors[node]-'a']++;
+        return count[node][colors[node]-'a'];
     }
 };
