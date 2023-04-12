56bbca60c3a96812b2d7d47f09c79fde7a2e4a33
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2023-04-12 17:39:34 +0200

    Time: 13 ms (11.27%), Space: 10.6 MB (46.42%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -5,43 +5,35 @@ public:
         string s="";
         for(int i=0; i<path.size(); i++)
         {   
-            if(path[i]!='/'&&i==path.size()-1)s+=path[i];
-            if((path[i]=='/'&&(s.size()!=0||i==0))||i==path.size()-1)
-            {   
-                if(s=="."){s="";continue;}
-                else if(s=="..")
+            if(path[i]=='/')continue;
+            s="";
+            while(i<path.size()&&path[i]!='/')
+            {
+                s+=path[i];
+                i++;
+            }
+            if(s==".")continue;
+            else if(s=="..")
                 {
                     if(directory.size())
                     {
                         directory.pop();
                     }
-                }else 
+             }else 
                 {   
                     if(s.size())
                     {directory.push(s);
                     }
-                }
-                s="";
-            }
-            else if(path[i]=='/'&&(s.size()==0))continue;
-            else 
-            {
-                s+=path[i];
-            }  
+                } 
         }
-        string ans="/";
+        string ans="";
         vector<string>pop_stack;
         while(directory.size())
         {
-            pop_stack.push_back(directory.top());
+            ans='/'+directory.top()+ans;
             directory.pop();
-        }int i=pop_stack.size()-1;
-        while(i>=0)
-        {
-            ans+=pop_stack[i];
-            if(i!=0) ans+='/';
-            i--;
         }
+        if(!ans.size()) ans+='/';
         return ans;
     }
 };
