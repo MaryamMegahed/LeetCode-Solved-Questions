877c3bf0ec025c561b7be7f84172c381070b2b25
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2023-04-23 02:05:13 +0200

    Time: 23 ms (99.14%), Space: 7.6 MB (78.46%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int lcs(string& s1, string& s2, int m, int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1));
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 || j == 0) {
-                    // One of the two strings is empty.
-                    dp[i][j] = 0;
-                } else if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    int minInsertions(string s) {
+        int dp[501][501],n=s.size();
+        int compare,end;
+        for(int i=0; i<n; i++)
+        {
+            dp[i][i]=1;
+        }
+        for(int i=0; i<n; i++)
+        {
+            compare=i+1;
+            for(int start=0; start<n; start++)
+            {   
+                end=start+compare;
+                if(end<n)
+                {
+                    if(s[start]==s[end])
+                    {
+                        dp[start][end]=2+dp[start+1][end-1];
+                    }else 
+                    {
+                        dp[start][end]=max(dp[start+1][end],dp[start][end-1]);
+                    }
                 }
             }
         }
-
-        return dp[m][n];
-    }
-
-    int minInsertions(string s) {
-        int n = s.length();
-        string sReverse = s;
-        reverse(sReverse.begin(), sReverse.end());
-
-        return n - lcs(s, sReverse, n, n);
+        return n-dp[0][n-1];
     }
 };
