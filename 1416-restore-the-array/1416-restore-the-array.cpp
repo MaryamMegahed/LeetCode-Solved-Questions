579cee90f8073396073334223bdb7dc7f5d76fb1
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2023-04-24 02:33:49 +0200

    Time: 68 ms (62.94%), Space: 11.9 MB (85.88%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -12,7 +12,6 @@ public:
             //addition=(s[start]-'0');
             for(int end=start,i=0; end<n&&i<=9; end++,i++)
             {
-                //if(s[end]-'0'>k)break;
                 current=s[end]-'0';
                 if(start==end){addition=current;}
                 else 
@@ -21,15 +20,6 @@ public:
                 dp[end+1]=(dp[start]+dp[end+1])%mod;
             }
         }
-        //  for(int i=0; i<n; i++){
-        //     for(int j=max(0, i-9); j<=i; j++){
-        //         string str=s.substr(j, i+1-j);
-        //         if(str[0]=='0' || (str.size()==10 && str[0]>'1'))
-        //             continue;
-        //         if(stoi(str)<=k)
-        //             dp[i+1]=(dp[i+1]+dp[j])%mod;
-        //     }
-        // }
         return dp[n];
     }
 };
