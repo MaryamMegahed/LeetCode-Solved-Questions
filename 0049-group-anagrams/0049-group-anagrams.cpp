19c3593c601c0f2bbf8df24e91678d4df05d2293
MaryamMegahed <37380802+MaryamMegahed@users.noreply.github.com>
2024-03-19 01:15:14 +0200

    Time: 43 ms (14.41%), Space: 32.4 MB (8.49%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
+      string signature(string s)
+      {   
+          vector<int>count(26,0);
+          for(int i=0; i<s.size(); i++)
+          {
+              count[s[i]-'a']++;
+          }
+          stringstream ss;
+          for(int i=0; i<26; i++)
+          {
+              if(count[i]!=0)
+              {
+                  ss<<char(i+'a')<<count[i];
+              }
+          }
+          return ss.str();
+      }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<int>taken(strs.size(),0);
-        //make a taken array 
-        //loop in array , check if taken in group or not
-        // if not taken-> build a group->put this index in it ->map ->iterate 
-
         vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
         for(int i=0; i<strs.size(); i++)
-        {
-            if(taken[i])
-            {   
-                continue;
-            }
-            unordered_map<char,int>mp,copy;
-            for(int j=0; j<strs[i].size(); j++)
-            {
-                mp[strs[i][j]]++;
-            }
-            vector<string>group;
-            group.push_back(strs[i]);
-            for(int j=i+1; j<strs.size(); j++)
-            {   
-                if(taken[j]||strs[j].size()!=strs[i].size()) continue;
-                copy=mp;
-                bool match=true;
-                for(int k=0; k<strs[j].size(); k++)
-                {
-                    if(copy[strs[j][k]]) 
-                    {
-                        copy[strs[j][k]]--;
-                    }else 
-                    { match=false;
-                        break;
-                    }
-                }
-                if(match) {group.push_back(strs[j]);
-                           taken[j]=1;               }
-            }
-            if(group.size())
-            {ans.push_back(group);
-            }
+        {   
+            mp[signature(strs[i])].push_back(strs[i]);
         }
-        return ans;
+        for(auto it=mp.begin(); it!=mp.end(); it++)
+        {
+            ans.push_back(it->second);
+        }
+            return ans;
+            
     }
 };
